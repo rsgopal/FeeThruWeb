e3f85580df0ee516c4c3effd6e7155a7ea5abd6a
B030771 <Ramamurthy.Gopalakrishnan@BNSF.com>
2018-09-30 15:03:02 -0500

    Auth0

---
+++ src/main/java/com/janakan/feethru/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.janakan.feethru.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.auth0.AuthenticationController;
+import com.janakan.feethru.AppConfig;
+
+@Controller
+public class LoginController {
+	@Autowired
+	private AuthenticationController controller;
+	@Autowired
+	private AppConfig appConfig;
+
+	@RequestMapping(value = CallbackController.LOGIN_PATH, method = RequestMethod.GET)
+	protected String login(final HttpServletRequest req) {
+		StringBuffer requestURL = req.getRequestURL();
+		StringBuffer redirectUrl = requestURL
+				.delete(requestURL.length() - req.getRequestURI().length(), requestURL.length()).append("/callback");
+		String authorizeUrl = controller.buildAuthorizeUrl(req, redirectUrl.toString())
+				.withAudience(String.format("https://%s/userinfo", appConfig.getDomain())).build();
+		return "redirect:" + authorizeUrl;
+	}
+}
